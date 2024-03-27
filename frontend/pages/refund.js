7d3b5350b3f1aed434458107ed74450d788169e1
Harshit <harshitbhardwaj001@gmail.com>
2024-03-27 18:16:13 +0530

    Minor Chnage

--- frontend/pages/refund.js
+++ frontend/pages/refund.js
@@ -7,14 +7,14 @@ const refund = () => {
         Cancellation and Refund Policy
       </h1>
       <p className="text-[2rem] leading-[3rem] text-center mt-[1rem] pb-[5rem] px-[2rem]">
-        UNFORTUNATELY, AS PER STORE POLICY, CANCELLATION AND REFUNDS ARE NOT
-        POSSIBLE AGAINST ANY ORDERS.
+        UNFORTUNATELY, AS PER STORE POLICY, CANCELLATION, REFUNDS AND RETURNS
+        ARE NOT POSSIBLE AGAINST ANY ORDERS.
       </p>
-      <p className="text-[2rem] leading-[3rem] text-center mt-[-2rem] pb-[5rem] px-[2rem]">
+      {/* <p className="text-[2rem] leading-[3rem] text-center mt-[-2rem] pb-[5rem] px-[2rem]">
         ALL EXCHANGES WILL UNDERGO A QUALITY CHECK BY OUR TEAM. WE RESERVE THE
         RIGHT TO DECLINE AN EXCHANGE IF: THE PRODUCT/S ARE RETURNED AFTER USE.
         THE PRODUCTS ARE DAMAGED.
-      </p>
+      </p> */}
     </div>
   );
 };
